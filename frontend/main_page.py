1cb35fcbf417454aa71b437d04cdd8afbf693394
andrushechka365 <142117922+andrushechka365@users.noreply.github.com>
2025-12-31 21:29:16 +0300

    minor changes

--- frontend/main_page.py
+++ frontend/main_page.py
@@ -1,28 +1,27 @@
 import streamlit as st
+from pathlib import Path
 
+st.markdown("### Вопрос по выбору для государственного экзамена по физике")
 
-st.markdown("## Вопрос по выбору для государственного экзамена по физике")
-st.markdown("### Тема: исследование поведения кубитов в переменном магнитном поле")
+img_path = Path(__file__).resolve().parent / "images" / "1.webp"
+st.image(str(img_path), caption="...", use_container_width=True)
 
+st.page_link("frontend/theory.py", label="➡️ Теоретическое введение")
+st.markdown("введение в предмет исследования")
 
-import streamlit as st
-
-st.markdown(
-    """
-### Навигация
-
-1. **Сфера Блоха** — демонстрация положения кубитов на сфере Блоха.
-2. **Распределение вероятностей** — симуляция распределения при измерении состояния кубитов (AerSimulator).
-3. **Теория** — введение в предмет исследования.
-4. **Принцип работы** — описание принципа работы симулятора.
-5. **Схема** — какими операциями с кубитами достигнуто данное положение.
-"""
-)
+st.page_link("frontend/how_work.py", label="➡️ Реализация")
+st.markdown("объяснение нюансов реализации")
 
 st.page_link("frontend/bloch_sphere.py", label="➡️ Сфера Блоха")
+st.markdown("демонстрация положения кубитов на сфере Блоха")
+
+
+
 st.page_link("frontend/stats.py", label="➡️ Распределение вероятностей")
-st.page_link("frontend/theory.py", label="➡️ Теория")
-# st.page_link("frontend/principle.py", label="➡️ Принцип работы")  # если есть файл
-st.page_link("frontend/scheme.py", label="➡️ Схема")
+st.markdown("симуляция распределения при измерении состояния кубитов")
+
+st.page_link("frontend/scheme.py", label="➡️ Логическая схема")
+st.markdown("схема, демонстрирующая, какими оперциями достигнуто положение кубитов")
+
 
 st.markdown("#### [Исходники](https://github.com/andrushechka37/Qubit-Simulation)")
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from qiskit.circuit.quantumcircuit import S
 import streamlit as st
 from backend.cubits import CircuitParams
 from backend.session_state import SessionState, update_circuit_instance
 
 
 def set_pages_st():
-    st.title("Симулятор для исследования поведения кубитов в переменном магнитном поле")
+    st.title("Симуляция поведения кубитов в переменном магнитном поле")
 
     main_page = st.Page("frontend/main_page.py", title="Главная страница")
+    theory = st.Page("frontend/theory.py", title="Теоретическое введение")
+    how_it_works = st.Page("frontend/how_work.py", title="Реализация")
     bloch_sphere = st.Page("frontend/bloch_sphere.py", title="Сфера Блоха")
     stats = st.Page("frontend/stats.py", title="Распределение вероятностей")
-    theory = st.Page("frontend/theory.py", title="Теория")
-    circuit_scheme = st.Page("frontend/scheme.py", title="Схема")
+    circuit_scheme = st.Page("frontend/scheme.py", title="Логическая схема")
 
-    pg = st.navigation([main_page, bloch_sphere, stats, theory, circuit_scheme])
+    pg = st.navigation([main_page, theory, how_it_works, bloch_sphere, stats, circuit_scheme])
     return pg
 
 
